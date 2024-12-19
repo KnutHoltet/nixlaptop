427782f3cfbee2ae0484c1f00fb4a4580879990f
KnutHoltet <knutholtet@live.no>
2024-12-19 14:06:41 +0000

    removing xdg-desktop-portal-hyprland

--- nixos/xdg-portal.nix
+++ nixos/xdg-portal.nix
@@ -2,6 +2,8 @@
   xdg.portal = {
     enable = true;
     extraPortals = 
-      [ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk ];
+      [  pkgs.xdg-desktop-portal-gtk ];
   };
+
+# pkgs.xdg-desktop-portal-hyprland
 }
