5a2174f678bbb36b0d6da00428d89d180d2963d6
KnutHoltet <knutholtet@live.no>
2024-12-19 14:12:47 +0000

    Adding xdg-desltop hyrpland

--- nixos/xdg-portal.nix
+++ nixos/xdg-portal.nix
@@ -1,9 +1,11 @@
 { pkgs, ... }: {
   xdg.portal = {
     enable = true;
+    xdgOpenUsePortal = true;
+    wlr.enable = true;
     extraPortals = 
-      [  pkgs.xdg-desktop-portal-gtk ];
+      [ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk ];
   };
 
-# pkgs.xdg-desktop-portal-hyprland
+# 
 }
