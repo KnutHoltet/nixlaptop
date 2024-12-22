5ad4400d18337d90590778d49f35caded4b67401
KnutHoltet <knutholtet@live.no>
2024-12-22 11:02:06 +0000

    Customizing waybar

--- home/core/system/bars/waybar.nix
+++ home/core/system/bars/waybar.nix
@@ -10,7 +10,22 @@
     programs.waybar = {
       enable = true;
       systemd.enable = true;
+      style = ''
+        * {
+          border: none;
+          border-radius: 0;
+          font-family: Source Code Pro;
+        }
+        window#waybar {
+          background: #16191C;
+          color: #AAB2BF;
+        }
+        #workspaces button {
+          padding: 0 10px;
+        }
+      '';
     };
+    
   
   };
  
