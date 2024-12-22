e796e576e357091b03a4cc55bee8895c2d13aa78
KnutHoltet <knutholtet@live.no>
2024-12-22 21:54:18 +0100

    adding comments about fonts

--- home/default.nix
+++ home/default.nix
@@ -6,7 +6,10 @@
   ];
   
 
+
   # fonts.fontconfig.defaultFonts.monospace
+  # May want to move the fonts to /nixos/ or nixos modules
+  # so you dont have to run `fc-cache -rf` to refresh the font cache everytime
   home.packages = with pkgs; [ nerd-fonts.jetbrains-mono ];
  
 
