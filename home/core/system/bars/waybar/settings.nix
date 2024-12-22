60f71c63244598de4847dd296fd4f9e0b8b42a9c
KnutHoltet <knutholtet@live.no>
2024-12-22 12:13:28 +0000

    Adding settings back waybar

--- home/core/system/bars/waybar/settings.nix
+++ home/core/system/bars/waybar/settings.nix
@@ -3,6 +3,13 @@
     mainBar = {
       layer = "top";
       position = "top";
+      height = 30;
+      output = [
+        "eDP-1"
+        "HDMI-A-1"
+      ];
+
+      modules-rigth = [ "temperatue" ];
     };
   };
 }
--- home/core/system/bars/waybar/waybar.nix
+++ home/core/system/bars/waybar/waybar.nix
@@ -2,7 +2,7 @@
   
   imports = [
     ./style.nix
-    # ./settings.nix
+    ./settings.nix
   ];
 
   options = {
