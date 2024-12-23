720ab40d3723efeb03fdd8d264dceee075da5e66
KnutHoltet <knutholtet@live.no>
2024-12-23 22:25:54 +0100

    fonts

--- home/default.nix
+++ home/default.nix
@@ -11,7 +11,7 @@
   # May want to move the fonts to /nixos/ or nixos modules
   # so you dont have to run `fc-cache -rf` to refresh the font cache everytime
   # run command before rebuild
-  home.packages = with pkgs; [ nerd-fonts ];
+  home.packages = with pkgs; [ nerdfonts ];
  
 
 
