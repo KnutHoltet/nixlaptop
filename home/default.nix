2c5f703970dd3ad34e4dc09915431be1f926f9f5
KnutHoltet <knutholtet@live.no>
2024-12-23 22:24:06 +0100

    fonts

--- home/default.nix
+++ home/default.nix
@@ -10,7 +10,8 @@
   # fonts.fontconfig.defaultFonts.monospace
   # May want to move the fonts to /nixos/ or nixos modules
   # so you dont have to run `fc-cache -rf` to refresh the font cache everytime
-  home.packages = with pkgs; [ nerdfonts ];
+  # run command before rebuild
+  home.packages = with pkgs; [ nerd-fonts ];
  
 
 
