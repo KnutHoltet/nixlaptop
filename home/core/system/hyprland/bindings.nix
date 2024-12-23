0f9e5c93442b6d4ed4c959d86049f894544d32f1
KnutHoltet <knutholtet@live.no>
2024-12-23 21:28:40 +0100

    trying to add resize

--- home/core/system/hyprland/bindings.nix
+++ home/core/system/hyprland/bindings.nix
@@ -23,6 +23,8 @@
       "$mod, j, movefocus, d" # Move Focus Down 
       "$mod, k, movefocus, u" # Move Focus Down 
 
+      "shiftMod,l, resize right 10"
+
 
       "$shiftMod, S, exec, ${pkgs.firefox}/bin/firefox https://www.google.com/search?q=$(wofi --show dmenu -L 1 -p 'Search on internet')"
 
