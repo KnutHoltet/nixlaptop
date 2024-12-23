b201dd9802ae93f7b5f09c00ea219e763245e1af
KnutHoltet <knutholtet@live.no>
2024-12-23 18:58:45 +0100

    adding style to waybar

---
+++ home/core/system/bars/waybar/oldStyle.nix
@@ -0,0 +1,26 @@
+{
+  programs.waybar = {
+    style = ''
+      * {
+        border: none;
+        border-radius: 0;
+        font-family: sans-serif;
+      }
+      window#waybar {
+        background: #16191C;
+        color: #AAB2BF;
+      }
+      #workspaces button {
+        padding: 0 20px;
+      }
+     
+
+     #custom-os_button {
+       font-family: "JetBrainsMono Nerd Font";
+     }
+      
+    '';
+  };
+}
+
+
