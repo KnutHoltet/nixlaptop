e9b469138986078b33d8380665350d104e574317
KnutHoltet <knutholtet@live.no>
2024-12-23 21:36:08 +0100

    trying to fix still

--- home/core/system/hyprland/bindings.nix
+++ home/core/system/hyprland/bindings.nix
@@ -23,7 +23,7 @@
       "$mod, j, movefocus, d" # Move Focus Down 
       "$mod, k, movefocus, u" # Move Focus Down 
 
-      "shiftMod, l, resizewindow, 10 0"
+      "shiftMod, l, resize, 10 0"
 
 
       "$shiftMod, S, exec, ${pkgs.firefox}/bin/firefox https://www.google.com/search?q=$(wofi --show dmenu -L 1 -p 'Search on internet')"
