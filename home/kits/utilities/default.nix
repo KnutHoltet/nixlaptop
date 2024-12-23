85f3eb2cbe694c372924a529ddb17cc57335409b
KnutHoltet <knutholtet@live.no>
2024-12-23 21:14:04 +0100

    adding utils packages

--- home/kits/utilities/default.nix
+++ home/kits/utilities/default.nix
@@ -7,7 +7,25 @@
 
 
   home.packages = with pkgs; [
+    brightnessctl
+
     zip
     unzip
+
+    qt5.qtwayland
+    qt6.qtwayland
+    libsForQt5.qt5ct
+    qt6ct
+
+    gnome-themes-extra
+
+    wayland-utils
+    wayland-protocols
+
+    direnv
+    glib
+
+    meson
+
   ];
 }
