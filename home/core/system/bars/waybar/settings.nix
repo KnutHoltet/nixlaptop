736ee2210190b48698d2e2f66be46cbbb1d41253
KnutHoltet <knutholtet@live.no>
2024-12-23 17:54:42 +0100

    Adding calendar for waybar

--- home/core/system/bars/waybar/settings.nix
+++ home/core/system/bars/waybar/settings.nix
@@ -106,17 +106,34 @@ in
        };
 
       
+      # TODO_COLORS
+      clock = {
+        format = "      {:%R\n %d.%m.%Y}";
+        tooltip-format = "<tt><small>{calendar}</small></tt>";
+        calendar = {
+          mode = "year";
+          mode-mon-col = 3;
+          week-pos = "right";
+          on-scroll = 1;
+          on-click-right = "mode";
+          format = {
+            months = "<span color='#ffead3'><b>{}</b></span>";
+            days = "<span color='#ecc6d9'><b>{}</b></span>";
+            weeks = "<span color='#99ffdd'><b>W{}</b></span>";
+            weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+            today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+
+          };
+
+        };
+      };
+
       
       battery = {
         timezone = "${timeZone}";
         format = "{capacity}% {icon}";
         format-alt = "{time} {icon}";
         format-charging = "{capacity}% lader";
-      };
-
-      clock = {
-        format-alt = "{:%Y-%m-%d}";
-        tooltip-format = "{:%Y-%m-%d | %H:%M}";
       };
 
       network = {
