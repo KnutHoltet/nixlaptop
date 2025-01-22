0d09c3a5264f524fa497121f3551e6cdeb390c98
KnutHoltet <knutholtet@live.no>
2025-01-22 15:27:00 +0100

    adding background options hyprlock

--- home/core/system/hyprlock/default.nix
+++ home/core/system/hyprlock/default.nix
@@ -13,6 +13,14 @@
         path = "$HOME/Downloads/black-and-white.png"; # TODO: only png supported for now
         # path = "";
         color = "rgba(25, 20, 20, 1.0)";
+
+        blur_passes = 0;
+        blur_size = 2;
+        noise  = 0;
+        contrast = 0;
+        brightness = 0;
+        vibrancy = 0;
+        vibrancy_darkness = 0.0;
  
 
       }; # background
