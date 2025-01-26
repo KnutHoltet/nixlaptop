814a4eb382bd816f0daf98dfc682a8122a6124a1
KnutHoltet <knutholtet@live.no>
2025-01-26 18:01:14 +0100

    adding

--- flake.nix
+++ flake.nix
@@ -41,8 +41,10 @@
     specialArgs =  { inherit inputs; };
     modules = [
      ./hosts/nullpointer/default.nix
+     ./hosts/nullpointer/home.nix
      inputs.home-manager.nixosModules.home-manager
      inputs.nvf.homeManagerModules.default
+
 
 
     ];
