b27d81ba18bf9e57abdae373cb9aa6f508dc736d
KnutHoltet <knutholtet@live.no>
2025-01-28 10:49:39 +0100

    new theme

--- home/kits/terminals/ghostty.nix
+++ home/kits/terminals/ghostty.nix
@@ -12,7 +12,7 @@
     programs.ghostty = {
       enable = true;
       settings = {
-        theme = "AtomOneLight";
+        theme = "catppuccin-frappe";
         keybind = [
           # "ctrl+h=goto_split:left"
           # "ctrl+l=goto_split:right"
