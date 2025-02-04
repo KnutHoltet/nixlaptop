0ccab75b226094a42d8ff2c004963b51c1155045
KnutHoltet <knutholtet@live.no>
2025-02-04 13:58:06 +0100

    m

--- home/core/system/hyprland/default.nix
+++ home/core/system/hyprland/default.nix
@@ -35,6 +35,10 @@ in
 
       input = {
         kb_layout = keyboardLayout;
+        
+        gesture_3flick_left = "workspace prev";
+        gesture_3flick_right = "workspace next"
+
       };
       
       exec-once = [
