0163c5be00af3f52f41e866b55030200e13f99b5
KnutHoltet <knutholtet@live.no>
2025-03-04 16:05:27 +0100

    moving folders

--- home/core/scripts/utils/default.nix
+++ home/core/scripts/utils/default.nix
@@ -1,17 +1,5 @@
-{ pkgs, ... }:
-
-let
-
-  count-lines = pkgs.writeShellScriptBin "count-lines" ''
-    if [ -z "$1" ]; then
-      echo "Please provide a file extension (e.g., '*.java')"
-      return 1
-    fi
-      wc -l $1 | awk '{s+=$1} END {print s}'
-  '';
-
-in {
-  home.packages = [ count-lines ];
-
+{
+  imports = [
+    ./cli-tools
+  ];
 }
-
