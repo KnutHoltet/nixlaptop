92422b216aeddf7b02c4f65998700e2c523709f0
KnutHoltet <knutholtet@live.no>
2025-09-28 16:54:12 +0200

    adding

--- home/core/scripts/swww/default.nix
+++ home/core/scripts/swww/default.nix
@@ -4,8 +4,7 @@ let
 
   swww-init = pkgs.writeShellScriptBin "swww-init" ''
     swww-daemon
-    swww img ~/Downloads/wallpapersden.com_makima-cool-chainsaw-man_2829x1545.jpg
-
+    swww img ~/Downloads/nios.png
   '';
 
 in {
