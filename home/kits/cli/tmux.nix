275d93566c02b04823f79237dcb1af60fae9f6bd
KnutHoltet <knutholtet@live.no>
2025-09-28 17:07:17 +0200

    adding

--- home/kits/cli/tmux.nix
+++ home/kits/cli/tmux.nix
@@ -6,7 +6,17 @@
   };
 
   config = lib.mkIf config.tmux.enable {
-    home.packages = with pkgs; [ tmux ];
+    programs.tmux = {
+      enable = true;
+      mouse = true;
+      keyMode = "vi";
+      extraConfig = ''
+        bind h select-pane -L
+	bind j select-pane -D
+	bind k select-pane -U
+	bind l select-pane -R
+      '';
+    };
   };
 
 }
